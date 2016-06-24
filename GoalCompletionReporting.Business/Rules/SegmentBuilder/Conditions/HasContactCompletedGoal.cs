5fc285090283b073d98aaec38ce6559da84a5283
Rob Earlam <robert.earlam@gmail.com>
2016-06-24 12:02:11 +1000

    Updated segment rule

---
+++ GoalCompletionReporting.Business/Rules/SegmentBuilder/Conditions/HasContactCompletedGoal.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq.Expressions;
+using Sitecore.Analytics.Rules.SegmentBuilder;
+using Sitecore.ContentSearch.Analytics.Models;
+using Sitecore.ContentSearch.Rules.Conditions;
+using Sitecore.Data;
+
+namespace GoalCompletionReporting.Business.Rules.SegmentBuilder.Conditions
+{
+    public class HasContactCompletedGoal<T> : TypedQueryableStringOperatorCondition<T, IndexedContact> where T : VisitorRuleContext<IndexedContact>
+    {
+        private const string IndexField = "Contact.CompletedGoals";
+        private const string ContainsOperatorId = "{2E67477C-440C-4BCA-A358-3D29AED89F47}";
+
+        public HasContactCompletedGoal()
+        {
+            this.OperatorId = ContainsOperatorId;
+        }
+
+        protected override Expression<Func<IndexedContact, bool>> GetResultPredicate(T ruleContext)
+        {
+            ID valueAsId;
+            var parsedValueToId = ID.TryParse(this.Value, out valueAsId);
+            ShortID valueAsShortId;
+            var parsedValueToShortId = ShortID.TryParse(this.Value, out valueAsShortId);
+
+            if (!parsedValueToId && !parsedValueToShortId)
+            {
+                return c => false;
+            }
+
+            if (parsedValueToShortId && valueAsId == (ID)null)
+            {
+                valueAsId = valueAsShortId.ToID();
+            }
+
+            var idConvertedToString = valueAsId.Guid.ToString("N").ToLowerInvariant();
+            return this.GetCompareExpression(c => c[IndexField], idConvertedToString);
+        }
+    }
+}
